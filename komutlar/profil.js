0cc1593844a971c79c259d259f403577eb22b56d
Glitch (v12-kayit-altyapi) <none>
2021-05-14 20:25:05 +0000

    🌹🐮 Checkpoint ./komutlar/profil.js:23040536/6010

--- komutlar/profil.js
+++ komutlar/profil.js
@@ -35,19 +35,6 @@ rozetler = true;
 .replace('BUGHUNTER_LEVEL_2', 'Bug Avcısı 2. Lvl')
 .replace('VERIFIED_BOT', 'Onaylı Bot');
 
-  let user = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
-let isim = message.mentions.members.first() || message.guild.members.get(args[0]);
-var sayi = 1 
-let data = db.get(`isim.${message.guild.id}`)
-let rol = db.fetch(`rol.${message.guild.id}`)
-if(!data) return message.channel.send( new Discord.MessageEmbed()
-    .setColor("0x2f3136") 
-    .setDescription(`
-      ${isim} Kullanıcısının İsim Geçmesi Aşağıda Yazmaktadır.`)
-    .setColor("0x2f3136"))
-let isimler = data.filter(x => x.userID === isim.id).map(x => `${sayi++}- \`• ${x.isim} | ${x.yas}\`  (<@&${x.role}>)\n`).join("\n")
-if(isimler === null) isimler = "Bu Kullanıcının Eski İsimleri Bulunamadı"
-if(isimler === undefined) isimler = "Bu Kullanıcının Eski İsimleri "
   
     const embed = new Discord.MessageEmbed()
     .setAuthor(kişi.user.username, kişi.user.avatarURL({dynamic:true}))
@@ -57,7 +44,6 @@ if(isimler === undefined) isimler = "Bu Kullanıcının Eski İsimleri "
     .addField('Durum', mention.presence.status.replace('online', 'Çevrimiçi').replace('idle', 'Boşta').replace('dnd', 'Rahatsız Etmeyin').replace('offline', 'Çevrimdışı'))
     .addField(`Hesap Kuruluş Tarihi`, `${moment(kişi.user.createdAt).format(" DD/MMMM/YYYY ")}`, true)
     .addField(`Sunucuya Giriş Tarihi\n`, `${moment(kişi.joinedTimestamp).format('D/MMMM/YYYY')}`, true)
-    .addField(`Eski İsimleri\n`, `${isimler}`)
     //.addField('Rozetler', `${rozetler ? mentionFlags : 'Yok'}`)
     //.addField('Roller', mentionMember.roles.cache.filter(a => a.name !== '@everyone').map(a => a).join(' ') ? mentionMember.roles.cache.filter(a => a.name !== '@everyone').map(a => a).join(' ') : 'Yok')
     .setFooter(`${message.author.tag} Tarafından İstendi.`)
