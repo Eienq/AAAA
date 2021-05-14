51c06e32da079968fb8012781818574748da1db7
Glitch (v12-kayit-altyapi) <none>
2021-05-14 10:18:43 +0000

    🎄🚍 Checkpoint ./komutlar/stats.js:23040536/1607

--- komutlar/stats.js
+++ komutlar/stats.js
@@ -1,25 +1,40 @@
-const Discord = require(`discord.js`);
-const db = require(`quick.db`)
+const Discord = require('discord.js');
+const db = require('quick.db')
 
 exports.run = async(client, message, args) => {
+  
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin Yeterli Yetkiye Sahip Değilsin!`);
 let kişi = message.mentions.users.first()
-if (!kişi) return message.reply("Lütfen Bir kullanıcı Belirtin.")
-    const erkekbilgi = await db.fetch(`erkekistatistik${kişi.id}.${message.guild.id}`)
-    const kızbilgi = await db.fetch(`kızistatistik${kişi.id}.${message.guild.id}`)
-    const toplambilgi = await db.fetch(`toplamistatistik${kişi.id}.${message.guild.id}`)
+if(!args[0]) {
+    const erkekbilgi = await db.fetch(`erkekistatistik${message.author.id}.${message.guild.id}`)
+    //const kızbilgi = await db.fetch(`kızistatistik${message.author.id}.${message.guild.id}`)
+    const toplambilgi = await db.fetch(`toplamistatistik${message.author.id}.${message.guild.id}`)
     const codework = new Discord.MessageEmbed()
-    .setAuthor(kişi.username, kişi.avatarURL())
-    .setThumbnail(kişi.avatarURL({dynamic:true}))
+    .setAuthor(message.author.username, message.author.avatarURL({dynamic:true}))
+    .setThumbnail(message.author.avatarURL({dynamic:true}))
     .setTimestamp()
     .setFooter(`${message.author.tag} Tarafından İstendi.`)
     .setDescription(`**Yetkilinin İstatistikleri**
     **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**
     **Toplam Kaydı \`${toplambilgi ? toplambilgi : '0'}\`**
     **Toplam Erkek Kaydı \`${erkekbilgi ? erkekbilgi : '0'}\`**
-    **Toplam Kadın Kaydı \`${kızbilgi ? kızbilgi : '0'}\`**
     **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**`)
-    message.channel.send(codework)
+    message.channel.send(codework)}
+  if(kişi) {
+     const erkekbilgi = await db.fetch(`erkekistatistik${kişi.id}.${message.guild.id}`)
+    //const kızbilgi = await db.fetch(`kızistatistik${kişi.id}.${message.guild.id}`)
+    const toplambilgi = await db.fetch(`toplamistatistik${kişi.id}.${message.guild.id}`)
+    const codework = new Discord.MessageEmbed()
+    .setAuthor(kişi.username, kişi.avatarURL)
+    .setThumbnail(message.mentions.users.first().avatarURL())
+    .setTimestamp()
+    .setFooter(`${message.author.tag} Tarafından İstendi.`)
+    .setDescription(`**Yetkilinin Bilgileri**
+    **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**
+    **Toplam Kaydı \`${toplambilgi ? toplambilgi : '0'}\`**
+    **Toplam Erkek Kaydı \`${erkekbilgi ? erkekbilgi : '0'}\`**
+    **▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬**`)
+    message.channel.send(codework)}  //**Toplam Kadın Kaydı \`${kızbilgi ? kızbilgi : '0'}\`**
 };
 exports.conf = {
  enabled: true,
