0162fd187b169f6795530e83be62f40ee238b209
Glitch (v12-kayit-altyapi) <none>
2021-05-15 20:02:13 +0000

    😬🐌 Checkpoint ./bot.js:23040536/2013 ./jkood.json:23040536/219

--- bot.js
+++ bot.js
@@ -165,14 +165,14 @@ client.on("guildMemberAdd", member => {
 
 //OTO İSİM SON
 
-//TAG ALANA ROL
+//TAG SİSTEMİ
 
 client.on("userUpdate", async (oldUser, newUser) => {
   if (oldUser.username !== newUser.username) {
-    let tag = "!"; //tagınız
-    let sunucu = "712978502176997380"; //sunucu ID
-    let kanal = "718761514542301237" //log kanal id
-    let rol = "752891156102250517"; //tag alınca verilcek rol id
+    let tag = jkood.tag;
+    let sunucu = jkood.sunucuid;
+    let kanal = jkood.taglog 
+    let rol = jkood.tagrol;
     if (newUser.username.includes(tag) && !client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
       client.channels.cache.get(kanal).send(`${newUser} **\`${tag}\`** tagını aldığı için <@&${rol}> rolünü kazandı!`)
       client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.add(rol)
@@ -184,7 +184,7 @@ client.on("userUpdate", async (oldUser, newUser) => {
   }
 })
 
-//TAG ALANA ROL
+//TAG SİSTEMİ
 
 //BOTU SESTE TUTMA
 
